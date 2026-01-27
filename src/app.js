fa7eae42c07a880abbebc569bba8a310be142839
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-01-27 10:03:20 +0700

    basic routing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/person", (req, res) => {
+  res.json({ name: "Rizki", age: 19 });
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
