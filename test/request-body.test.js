24356bac87cd204e47da136125e961b64010fc7d
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-01-30 09:47:37 +0700

    request body

---
+++ test/request-body.test.js
@@ -0,0 +1,23 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/products/add", (req, res) => {
+  const { name, price } = req.body;
+
+  res.json({ name, price });
+});
+
+test("Test Request Body", async () => {
+  const res = await request(app)
+    .post("/products/add")
+    .send({ name: "Mangga", price: 10000 })
+    .set("Accept", /json/);
+
+  expect(res.body).toMatchObject({ name: "Mangga", price: 10000 });
+  expect(res.body.name).toBe("Mangga");
+  expect(res.body.price).toBe(10000);
+});
